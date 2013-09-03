c828c47bff7a4283a48e39705f24b8bf400b9ffa
nagumitti <nagaraj@telibrahma.com>
2013-09-03 18:55:12 +0530

    Update pyquerydemo.py

--- pyquerydemo.py
+++ pyquerydemo.py
@@ -1,9 +1,6 @@
 from pyquery import PyQuery as pq
 
-from pyquery.test import path_to_html_file
-
 def pyquery():
-    
     rinfo=pq(url="http://erail.in/station/SBC?filter=4")
     for x in rinfo("table")("tr"):
         print "Train No.- "+str(rinfo(x)("a").text())+" Train Name- "+str(rinfo(x)("td").eq(2).text())+" Type- "+str(rinfo(x)("td").eq(3).text())
