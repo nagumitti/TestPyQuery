b43b63e8c2aaba906c2cd6be1654c80ae48687a6
Nagaraj <nagaraj@telibrahma.com>
2013-09-04 17:39:07 +0530

    Update pyquerydemo.py

--- pyquerydemo.py
+++ pyquerydemo.py
@@ -1,6 +1,22 @@
 from pyquery import PyQuery as pq
 
 def pyquery():
+    tno=[];tname=[];tarr=[];tdep=[];ttype=[];
     rinfo=pq(url="http://erail.in/station/SBC?filter=4")
     for x in rinfo("table")("tr"):
-        print "Train No.- "+str(rinfo(x)("a").text())+" Train Name- "+str(rinfo(x)("td").eq(2).text())+" Type- "+str(rinfo(x)("td").eq(3).text())
+        tno.append(str(rinfo(x)("a").text()))
+        tname.append(str(rinfo(x)("td").eq(2).text()))
+        ttype.append(str(rinfo(x)("td").eq(3).text()))
+        tarr.append(str(rinfo(x)("td").eq(4).text()))
+        tdep.append(str(rinfo(x)("td").eq(5).text()))
+    tno.pop(0);tname.pop(0);ttype.pop(0);tarr.pop(0);tdep.pop(0)
+    print tno
+    print tname
+    print ttype
+    print tarr
+    print tdep
+    stationInfo=pq(url="http://erail.in/js/cmp/Stations.js")
+    s=str(stationInfo("p").text())
+    slist= s.split(";")
+    stationList=slist[0][slist[0].find("\"")+1:-1].split(",")
+    print stationList
